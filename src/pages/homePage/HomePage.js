69af0b28ffd04e7519ddf9027563ca3699ad22ad
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2021-10-27 14:15:01 +0300

    add media screen CalloriesTextStyled

--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import { Button } from '../../components/button/Button';
-import { HomePageStyled } from './HomePageStyled';
+import React from "react";
+import { Button } from "../../components/button/Button";
+import CalloriesText from "../../components/calloriesText/CalloriesText";
+import { HomePageStyled } from "./HomePageStyled";
 // import styles from "./HomePage.module.css"
 
 const HomePage = () => {
-    return (
-            <div className='bg-img'>
-            <div className="container">
-            <HomePageStyled>
-            <h1 className='home__title'>Просчитай свою суточную норму калорий прямо сейчас</h1>          
-            <div className="home__btn"><Button  buttonName = "Похудеть"/></div>
-            </HomePageStyled>
-            </div>
-            </div>
-    );
-}
+  return (
+    <div className="bg-img">
+      <div className="container">
+        <CalloriesText />
+        <HomePageStyled>
+          <h1 className="home__title">
+            Просчитай свою суточную норму калорий прямо сейчас
+          </h1>
+          <div className="home__btn">
+            <Button buttonName="Похудеть" />
+          </div>
+        </HomePageStyled>
+      </div>
+    </div>
+  );
+};
 
 export default HomePage;
