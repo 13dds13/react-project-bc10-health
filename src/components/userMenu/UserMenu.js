5d2c2ee1cef2e6cb8f71c2c001ca9cd7c3c09e34
Anton <a.sharudylo@gmail.com>
2021-10-28 09:59:13 +0300

    feature

--- src/components/userMenu/UserMenu.js
+++ src/components/userMenu/UserMenu.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { UserMenuStyled } from "./UserMenuStyled";
 import { authLogout } from "../../redux/auth/authOperations";
-
+import { getUserName } from "../../redux/auth/authSelectors";
 
 const UserMenu = () => {
 
@@ -10,7 +10,7 @@ const dispatch = useDispatch();
 
 const onHandleClick = () => dispatch(authLogout())
 
-  const userName = useSelector(state => state.authData.userInfo.user?.username)
+  const userName = useSelector(getUserName)
   return <UserMenuStyled>
     <p className='user-menu__text'>{userName}</p>
     <button className='user-menu__button' onClick={onHandleClick}>Выйти</button>
--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,3 +1,4 @@
 export const getIsAuth = (state) => state.auth.isAuth;
 export const getRefreshToken = (state) => state.auth.authData.refreshToken;
 export const getSid = (state) => state.auth.authData.sid;
+export const getUserName = (state) => state.auth.authData.username;
