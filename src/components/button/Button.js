59c4cb2901bc0ec7580fed8963954998584ea71a
Ivan <vanuhamark@gmail.com>
2021-10-28 18:16:06 +0300

    add data

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,16 +1,26 @@
-import React from 'react'
-import styles from "./Buttot.module.css"
+import React from "react";
+import sprite from "../../images/sprite.svg";
+import styles from "./Buttot.module.css";
 
-const Button = ({buttonName}) => {
-    return (
-        <button className={`${styles.btn} ${styles.btn_orange}`} type="button" > {buttonName} </button>
-    );
-}
+const Button = ({ buttonName, type }) => {
+  return (
+    <button className={`${styles.btn} ${styles.btn_orange}`} type={type}>
+      {buttonName}
+    </button>
+  );
+};
 
-const ButtonAdd = ({buttonNameAdd}) => {
-    return (
-        <button className={`${styles.btn_add} ${styles.btn_orange_add}`} type="button" > {buttonNameAdd} <span className={styles.add}>+</span> </button>
-    );
-}
+const ButtonAdd = ({ type }) => {
+  return (
+    <button
+      className={`${styles.btn_add} ${styles.btn_orange_add}`}
+      type={type}
+    >
+      <svg width="20" height="20" fill="white">
+        <use href={sprite + "#add"} />
+      </svg>
+    </button>
+  );
+};
 
-export  {ButtonAdd, Button};
+export { ButtonAdd, Button };
