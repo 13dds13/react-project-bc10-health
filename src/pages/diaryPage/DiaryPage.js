990cb612ec22f28b6586889585cc14d14b281000
Anton <a.sharudylo@gmail.com>
2021-10-28 20:20:08 +0300

    fix

--- src/pages/diaryPage/DiaryPage.js
+++ src/pages/diaryPage/DiaryPage.js
@@ -1,10 +1,11 @@
 import ProductForm from "../../components/productForm/ProductForm";
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
-import "./react-datapicker.css"
+import "./react-datapicker.css";
 import sprite from "../../images/sprite.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { setDiaryValue } from "../../redux/isOpenModalForDiaryMobilePage/diaryModalAction";
+import Modal from "../../components/modal";
 
 const DiaryPage = () => {
   const [startDate, setStartDate] = useState(new Date());
@@ -17,22 +18,25 @@ const DiaryPage = () => {
     <>
       <h2>DiaryPage</h2>
       <div className={"dataPicker__box"}>
-      <DatePicker dateFormat="dd.MM.yyyy" selected={startDate} onChange={(date) => setStartDate(date)} />
-      <svg className="dataPicker__svg" width="18" height="20">
-        <use href={sprite + "#calendar"}/>
-      </svg>
-   <ProductForm />
+        <DatePicker
+          dateFormat="dd.MM.yyyy"
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+        />
+        <svg className="dataPicker__svg" width="18" height="20">
+          <use href={sprite + "#calendar"} />
+        </svg>
+        <ProductForm />
 
-   <button type="button" onClick={onHandleCliсk}>
-        openModal
-      </button>
-      {isModalOpen && (
-        <Modal hideModal={onHandleCliсk}>
-          <h2>тут форма</h2>
-        </Modal>
-      )}
-</div>
->>>>>>> dev
+        <button type="button" onClick={onHandleCliсk}>
+          openModal
+        </button>
+        {isModalOpen && (
+          <Modal hideModal={onHandleCliсk}>
+            <h2>тут форма</h2>
+          </Modal>
+        )}
+      </div>
     </>
   );
 };
