9b52f73175d33a4505013c5f2ac4dcc03e0b71cc
frandrew90 <frandrewjs@gmail.com>
2021-10-29 18:58:35 +0300

    made redirect on registration btn

--- src/components/authTempForm/AuthTempForm.js
+++ src/components/authTempForm/AuthTempForm.js
@@ -2,16 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 import { mainRoutes } from "../../routes/mainRoutes";
 import { Formik } from "formik";
-import { RegistrationFormStyled } from "./RegistrationForm.styled";
+import { AuthFormStyled } from "./AuthTempForm.styled";
 import {
   validationsSchemaRegistration,
   validationsSchemaSignIn,
 } from "./validationSchema";
 import { Button } from "../button/Button";
+import { useHistory } from "react-router-dom";
 
 const AuthForm = ({ handleSubmit, btnName }) => {
+  const history = useHistory();
+
   return (
-    <RegistrationFormStyled>
+    <AuthFormStyled>
       <form className="registration-form">
         <h1 className="registration-form__title">{btnName}</h1>
         <Formik
@@ -144,10 +147,13 @@ const AuthForm = ({ handleSubmit, btnName }) => {
                 {btnName === mainRoutes[3].name && (
                   <div className="registration-form__second-btn-wrapper">
                     <Button
-                      isValid={isValid}
-                      dirty={dirty}
-                      onClick={handleSubmit}
-                      type={`submit`}
+                      isValid={true}
+                      dirty={true}
+                      onClick={() => {
+                        let path = `/singup`;
+                        history.push(path);
+                      }}
+                      type={`button`}
                       className="registration-form__second-btn"
                       buttonName={`Регистрация`}
                       btn_mod="btn_white"
@@ -159,7 +165,7 @@ const AuthForm = ({ handleSubmit, btnName }) => {
           )}
         </Formik>
       </form>
-    </RegistrationFormStyled>
+    </AuthFormStyled>
   );
 };
 
