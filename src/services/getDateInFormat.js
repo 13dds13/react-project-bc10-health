ac0d38ca278465d503093405315de22b4e4d5c81
Dmitry Dovgopol <dovgopol.ds@gmail.com>
2021-11-02 08:39:36 +0200

    fixed bugs

--- src/services/getDateInFormat.js
+++ src/services/getDateInFormat.js
@@ -1,9 +1,7 @@
 const getDateInFormat = (dateToChange) => {
-  let date = dateToChange.getDate();
-  let month = dateToChange.getMonth() + 1;
+  const date = dateToChange.getDate().toString().padStart(2, "0");
+  const month = (dateToChange.getMonth() + 1).toString().padStart(2, "0");
   const year = dateToChange.getFullYear();
-  if (date < 10) date = `0${date}`;
-  if (month < 10) month = `0${month}`;
 
   return `${year}-${month}-${date}`;
 };
